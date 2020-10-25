d5ea6c4fa54defae03c336938ab9f054fecac690
Mike Turano <mike.turano@gmail.com>
2020-10-25 10:16:01 -0500

    enemies change in levels

--- src/levels/levels.js
+++ src/levels/levels.js
@@ -7,30 +7,30 @@ const levels = {
   },
   2: {
     size: 12,
-    chumps: 1,
-    slimes: 1
+    chumps: 2,
+    slimes: 0
   },
   3: {
     size: 12,
     chumps: 2,
     slimes: 1,
-    spiders: 1
+    spiders: 0
   },
   4: {
     size: 12,
-    chumps: 1,
-    slimes: 3,
-    spiders: 0
+    chumps: 2,
+    slimes: 1,
+    spiders: 1
   },
   5: {
     size: 14,
-    chumps: 1,
+    chumps: 2,
     slimes: 1,
-    spiders: 3
+    spiders: 2
   },
   6: {
     size: 16,
-    chumps: 3,
+    chumps: 2,
     slimes: 2,
     spiders: 2
   }
