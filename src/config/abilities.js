22060a6e8fd4b34e7977edc238dee388569990b2
Mike Turano <mike.turano@gmail.com>
2020-10-31 02:09:52 -0500

    ability descriptions

--- src/config/abilities.js
+++ src/config/abilities.js
@@ -7,10 +7,10 @@ class Ability {
 }
 
 const abilities = [
-  new Ability('Bleed', 5, 'Increases blood gain, +1 damage / 3 points'),
-  new Ability('Fire', 10, 'Deals direct damage, +1 / point'),
-  new Ability('Ice', 15, 'Stuns opponents, 1 turn +1 turn / 2 add\'l points'),
-  new Ability('Size', 20, 'Increases reach by +1 tile / point')
+  new Ability('Bleed', 5, 'More blood gain (+1/pt), some damage (+1/3 pts)'),
+  new Ability('Fire', 10, 'More attack damage (+1/pt)'),
+  new Ability('Ice', 15, 'Stuns opponents (1 turn +1 turn / 2 add\'l pts)'),
+  new Ability('Size', 20, 'Increases reach (+1 tile / pt)')
 ];
 
 export { Ability, abilities };
