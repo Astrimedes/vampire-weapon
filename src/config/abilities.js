a2427f7cfd0822a77836c0eac2d5292ceca3d814
Mike Turano <mike.turano@gmail.com>
2020-11-29 01:27:40 -0600

    higher ability costs

--- src/config/abilities.js
+++ src/config/abilities.js
@@ -19,9 +19,9 @@ class Reach extends Ability {
 
 const Abilities = [
   new Ability('Bleed', 5, 'More blood gain (+1/pt), some damage (+1/3 pts)'),
-  new Ability('Fire', 10, 'More attack damage (+1/pt)'),
-  new Ability('Ice', 15, 'Stuns opponents (1 turn +1 turn / 2 add\'l pts)'),
-  new Reach('Size', 20, 'Increases reach (+1 tile/pt)')
+  new Ability('Fire', 50, 'More attack damage (+1/pt)'),
+  new Ability('Ice', 50, 'Stuns opponents (1 turn +1 turn / 2 add\'l pts)'),
+  new Reach('Size', 100, 'Increases reach (+1 tile/pt)')
 ];
 
 export { Ability, Abilities };
